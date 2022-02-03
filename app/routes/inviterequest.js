49efbf91850b910c8afb7958f34b65e56d1b6eb2
beckysarahlee <beckylee.digitaldwp@gmail.com>
2022-02-03 19:41:39 +0000

    New prototype for invite request

---
+++ app/routes/inviterequest.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router()
+
+
+// Invite code or GOV.UK Verify
+
+router.post('/invite-request/confirm-identity', function(req, res) {
+  if (req.body['confirm-identity'] === 'code') {
+    res.redirect('invite-code');
+  } else {
+    res.redirect('your-name');
+  }
+});
+
+  module.exports = router;
