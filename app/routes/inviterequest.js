ca85a3367cd4e660f868282ce32f17ba4f1de6a8
kieranwyatt <kieran.wyatt@engineering.digital.dwp.gov.uk>
2022-02-15 18:20:46 +0000

    file renaming on invitation code journey

--- app/routes/inviterequest.js
+++ app/routes/inviterequest.js
@@ -4,11 +4,11 @@ const router = express.Router()
 
 // Invite code or GOV.UK Verify
 
-router.post('/invite-request/confirm-identity', function(req, res) {
+router.post('/invite-request/invitation-code', function(req, res) {
   if (req.body['confirm-identity'] === 'code') {
     res.redirect('invite-code');
   } else {
-    res.redirect('invite-code-request');
+    res.redirect('request-invitation-code');
   }
 });
 
