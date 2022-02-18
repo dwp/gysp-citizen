70534e0ac5d5eed33d186d3f9c6788b8c612ec07
beckysarahlee <beckylee.digitaldwp@gmail.com>
2022-02-18 14:57:48 +0000

    Making most things work

--- app/routes/inviterequest.js
+++ app/routes/inviterequest.js
@@ -12,4 +12,28 @@ router.post('/invite-request/invitation-code', function(req, res) {
   }
 });
 
+
+// Invite to name
+router.post('/invite-request/request-invitation-code', (req, res) => {
+  res.redirect('/invite-request/your-name')
+})
+;
+
+// Name to DOB
+router.post('/invite-request/your-name', (req, res) => {
+  res.redirect('/invite-request/date-of-birth')
+})
+;
+
+// DOB for too young
+
+
+router.post('/invite-request/date-of-birth', function(req, res) {
+  if (req.session.data["year"] === "1957") {
+    res.redirect('cannot-request-code');
+  } else {
+    res.redirect('home-address');
+  }
+});
+
   module.exports = router;
