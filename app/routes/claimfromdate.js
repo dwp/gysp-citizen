baf7caa64e67fd6a11e392e91edf46d89a8a5f1c
beckysarahlee <beckylee.digitaldwp@gmail.com>
2022-06-28 21:08:01 +0100

    Setting up scenarios

---
+++ app/routes/claimfromdate.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const router = express.Router()
+
+
+
+// Scenario one
+
+router.post('/claimfromdate/s1/check-start-date', function(req, res) {
+  if (req.body['claim-from'] === 'delay') {
+    res.redirect('');
+  } else {
+    res.redirect('');
+  }
+});
+
+
+// Scenario two
+
+
+// Scenario three
+
+
+
+
+
+
+
+
+
+router.post('/additionalsecurity/marital-status', function(req, res) {
+  if (req.body['marital'] === 'married') {
+    res.redirect('marital-details');
+  }
+  else if (req.body['marital'] === 'divorced') {
+    res.redirect('marital-details-divorce');
+  }
+  else if (req.body['marital'] === 'civil') {
+    res.redirect('marital-details-civil-partner');
+  }
+  else if (req.body['marital'] === 'dissolved') {
+    res.redirect('marital-details-ex-civil-partner');
+  }
+  else if (req.body['marital'] === 'widowed') {
+    res.redirect('marital-details-late-spouse');
+  }
+  else {
+    res.redirect('/additionalsecurity/contact');
+  }
+});
+
+
+router.post('/additionalsecurity/marital-details-divorce', (req, res) => {
+  res.redirect('/additionalsecurity/ex-spouse-details')
+})
+;
+
+r
+
+
+  // Alt formats yes/no
+  router.post('/additionalsecurity/question-3', function(req, res) {
+    if (req.body['security-question-three'] === '10000') {
+      res.redirect('check-successful');
+    } else {
+      res.redirect('check-failed');
+    }
+  });
+
+
+
+  module.exports = router;
