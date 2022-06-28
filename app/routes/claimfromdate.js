cc8cd4d72070ae58a535965ce2f1ad0f43365fdc
beckysarahlee <beckylee.digitaldwp@gmail.com>
2022-06-28 22:03:18 +0100

    Claim from

--- app/routes/claimfromdate.js
+++ app/routes/claimfromdate.js
@@ -5,6 +5,12 @@ const router = express.Router()
 
 // Scenario one
 
+router.post('/claimfromdate/s1/date-want-sp', (req, res) => {
+  res.redirect('/claimfromdate/s1/check-start-date')
+})
+;
+
+
 router.post('/claimfromdate/s1/check-start-date', function(req, res) {
   if (req.body['claim-from'] === 'delay') {
     res.redirect('');
