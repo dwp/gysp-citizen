ecb4f53d84eb99a4276dfbca4d251c8c7c538b10
beckysarahlee <beckylee.digitaldwp@gmail.com>
2022-06-29 11:49:55 +0100

    Tring to find things...

--- app/routes/claimfromdate.js
+++ app/routes/claimfromdate.js
@@ -29,43 +29,4 @@ router.post('/claimfromdate/s1/check-start-date', function(req, res) {
 
 
 
-
-
-
-
-router.post('/additionalsecurity/marital-status', function(req, res) {
-  if (req.body['marital'] === 'married') {
-    res.redirect('marital-details');
-  }
-  else if (req.body['marital'] === 'divorced') {
-    res.redirect('marital-details-divorce');
-  }
-  else if (req.body['marital'] === 'civil') {
-    res.redirect('marital-details-civil-partner');
-  }
-  else if (req.body['marital'] === 'dissolved') {
-    res.redirect('marital-details-ex-civil-partner');
-  }
-  else if (req.body['marital'] === 'widowed') {
-    res.redirect('marital-details-late-spouse');
-  }
-  else {
-    res.redirect('/additionalsecurity/contact');
-  }
-});
-
-
-
-
-  // Alt formats yes/no
-  router.post('/additionalsecurity/question-3', function(req, res) {
-    if (req.body['security-question-three'] === '10000') {
-      res.redirect('check-successful');
-    } else {
-      res.redirect('check-failed');
-    }
-  });
-
-
-
   module.exports = router;
