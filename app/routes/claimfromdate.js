4b1fc467a45fb5645bc4504e9f119ace0239754c
Al Robinson <77453029+Al-Robinson@users.noreply.github.com>
2022-07-04 15:35:23 +0100

    ur changes

--- app/routes/claimfromdate.js
+++ app/routes/claimfromdate.js
@@ -19,7 +19,19 @@ router.post('/claimfromdate/s1/check-start-date', function(req, res) {
   }
 });
 
-router.post('/claimfromdate/scenario-1/date-want-sp', function(req, res) {
+router.post('/claimfromdate/scenario/date-want-sp', function(req, res) {
+  if (req.body['passport-issued-month'] === '11') {
+    res.redirect('do-not-claim-yet');
+  }
+  else if (req.session.data["passport-issued-month"] === "10") {
+    res.redirect('prison');
+  } 
+  else if (req.session.data["passport-issued-month"] === "9") {
+    res.redirect('check-start-date');
+  }
+});
+
+router.post('/claimfromdate/scenario/post-spa', function(req, res) {
   if (req.body['passport-issued-month'] === '11') {
     res.redirect('do-not-claim-yet');
   }
@@ -32,14 +44,14 @@ router.post('/claimfromdate/scenario-1/date-want-sp', function(req, res) {
 });
 
 router.post('/claimfromdate/scenario-3/post-spa', function(req, res) {
-  if (req.body['passport-issued-month'] === '11') {
-    res.redirect('do-not-claim-yet');
+  if (req.body['passport-issued-year'] === '2021') {
+    res.redirect('check-start-date');
   }
-  else if (req.session.data["passport-issued-month"] === "10") {
+  else if (req.session.data["passport-issued-year"] === "2022") {
     res.redirect('prison');
   } 
-  else if (req.session.data["passport-issued-month"] === "9") {
-    res.redirect('check-start-date');
+  else {
+    res.redirect('do-not-claim-yet');
   }
 });
 
