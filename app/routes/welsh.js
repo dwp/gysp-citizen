3b6c79b2d24ae61de8394a5b8cbf0366b1b8862d
kieranwyatt <kieran.wyatt@engineering.digital.dwp.gov.uk>
2022-07-27 15:26:40 +0100

    Welsh routing - I am a genius

--- app/routes/welsh.js
+++ app/routes/welsh.js
@@ -2,6 +2,23 @@ const express = require('express');
 const router = express.Router()
 
 
+
+router.post('/welsh/contact', (req, res) => {
+  res.redirect('/welsh/language-speak')
+})
+;
+
+router.post('/welsh/language-speak', (req, res) => {
+  res.redirect('/welsh/language-write')
+})
+;
+
+router.post('/welsh/large-print', (req, res) => {
+  res.redirect('/welsh/bank')
+})
+;
+
+
 // Language write to large print
 
 router.post('/welsh/language-write', function(req, res) {
@@ -9,6 +26,16 @@ router.post('/welsh/language-write', function(req, res) {
     res.redirect('alt-formats');
   } else {
     res.redirect('large-print');
+  }
+});
+
+// Alternative formats
+
+router.post('/welsh/alt-formats', function(req, res) {
+  if (req.body['alt-formats'] === 'yes') {
+    res.redirect('alt-formats-choice');
+  } else {
+    res.redirect('bank');
   }
 });
 
